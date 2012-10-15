91c17b33189c03701e9fa0eb8fe3b895d1b3d53f
Patrik Wibron <patrik@wibron.nu>
2012-10-15 20:44:45 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Facebook Icon-remover
+
+An extension that removes all the ugly icons on facebook. 
+
+Enjoy.
---
+++ main.js
@@ -0,0 +1,14 @@
+var icons, remove;
+
+remove = function() {
+  icons = document.querySelectorAll('.emote_img');
+  iconTexts = document.querySelectorAll('.emote_text');
+  for (var i = 0; i<icons.length; i++) {
+    icons[i].style.display = 'none';
+  }
+  for (var j = 0; j < iconTexts.length; j++) {
+    iconTexts[j].style.display = 'inline';
+  }
+};
+
+setInterval(remove, 300);
