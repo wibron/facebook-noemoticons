a0dfbff11130dd5887f56f33e1a3cc2ccd9293ae
Patrik Wibron <patrik@wibron.nu>
2012-10-16 09:56:55 +0200

    Change variable declaration and fixed spaces

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-var icons, remove;
+var icons, iconTexts, remove;
 
 remove = function() {
   icons = document.querySelectorAll('.emote_img');
   iconTexts = document.querySelectorAll('.emote_text');
-  for (var i = 0; i<icons.length; i++) {
+  for (var i = 0; i < icons.length; i++) {
     icons[i].style.display = 'none';
   }
   for (var j = 0; j < iconTexts.length; j++) {
